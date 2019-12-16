39124c81c376992d9478fc5e13e5af7e252d9c9a
Ansar <ansarnisargill@gmail.com>
2019-12-15 21:16:02 -0800

    almost set up

--- src/app/Services/portfolio.service.ts
+++ src/app/Services/portfolio.service.ts
@@ -8,16 +8,16 @@ export class PortfolioService {
   public projects: Project[] = [];
   constructor() { }
   getAllProjects(): Project[] {
-    for (let i = 1; 1 < 5; i++) {
+    for (let i = 1; i<5; i++) {
       let p = new Project();
       p.demoUrl = 'https://irradiated-paint.glitch.me/';
       p.githubUrl = 'https://github.com/ansarnisargill/PlaneConvergenceProject/tree/master/AirportProject';
       p.id = i.toString();
       p.desktopSupport = true;
       p.mobileSupport = true;
-      p.pictureUrl = 'https://images.unsplash.com/photo-1574170240473-5733a5b43a3c?ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80';
+      p.pictureUrl = 'https://bulmatemplates.github.io/bulma-templates/images/admin.png';
       p.title = "Map Showing Direction";
-      p.version = '1.0.0';
+      p.version = 'Leaflet, Javascript';
       this.projects.push(p);
     }
     return this.projects;
